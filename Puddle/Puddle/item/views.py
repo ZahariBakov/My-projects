af59dc9fec669c0c5bff7dd9740314856091ad99
ZahariBakov <zaharibakov@gmail.com>
2023-05-15 17:25:29 +0300

    Update Django project "Puddle". Add "Edit items".

--- Puddle/Puddle/item/views.py
+++ Puddle/Puddle/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from Puddle.item.forms import NewItemForm
+from Puddle.item.forms import NewItemForm, EditItemForm
 from Puddle.item.models import Item
 
 
@@ -33,6 +33,27 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New item',
+    })
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
 
 
